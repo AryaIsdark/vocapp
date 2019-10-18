858cff74af0c2395621ff5c6fc1c9e1548024c4b
AryaShahali <ars@bankingcircle.com>
2019-10-18 14:23:53 +0200

    consolidated unit test for loadData in posts/actions

--- src/store/posts/actions.test.tsx
+++ src/store/posts/actions.test.tsx
@@ -1,6 +1,7 @@
 import * as actions from "./actions"
 import {ApiOkResponse} from 'apisauce'
 import * as api from 'api/apiFunctions'
+import { ActionTypes } from "./types";
 
 jest.mock('api/apiFunctions', () => ({
   getPosts: jest.fn()
@@ -8,12 +9,12 @@ jest.mock('api/apiFunctions', () => ({
 
 describe("store/posts/actions", () => {
   it("can load data", async() => {
-
-    const getpostsSpy = jest
+    const mockData = [{id:1,title:'testPost'}]
+    const getPostsSpy = jest
       .spyOn(api, 'getPosts')
       .mockImplementation(() =>
           Promise.resolve({
-            data: [{id:1,title:'testPost'}],
+            data: mockData,
           } as ApiOkResponse<any>),
         );
 
@@ -21,9 +22,13 @@ describe("store/posts/actions", () => {
 
     await actions.loadData()(dispatch)
 
+    expect(dispatch).toHaveBeenCalledWith(actions.setHasError(false));
+    expect(dispatch).toHaveBeenCalledWith(actions.setIsLoading(true));
+    expect(dispatch).toHaveBeenCalledWith({type:ActionTypes.SET_DATA, payload: {data: mockData} })
+    expect(dispatch).toHaveBeenCalledWith(actions.setIsLoading(false));
     expect(dispatch).toHaveBeenCalledTimes(4);
     
-    getpostsSpy.mockRestore()
+    getPostsSpy.mockRestore()
 
   });
 });
