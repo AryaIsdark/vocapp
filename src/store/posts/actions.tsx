4355f98131d1a8b52c7edc3f8d8d651d3b2fb0d1
AryaShahali <ars@bankingcircle.com>
2019-10-18 14:16:09 +0200

    Added setIsLoading and setHasError in posts/actions

--- src/store/posts/actions.tsx
+++ src/store/posts/actions.tsx
@@ -1,22 +1,36 @@
-import {ActionTypes, Action} from './types'
+import { ActionTypes, Action } from './types'
 import * as api from 'api/apiFunctions'
 import { Dispatch } from 'react';
 
 
 export const loadData = (params = {}) => async (dispatch: Dispatch<Action>) => {
-    // dispatch(setIsLoading(true));
-  
+    dispatch(setHasError(false))
+    dispatch(setIsLoading(true))
     try {
-      const response = await api.getPosts();
+        const response = await api.getPosts(params);
         console.log(response)
-      dispatch({
-        type: ActionTypes.SET_DATA,
-        payload: { data: response.data },
-      });
+        dispatch({
+            type: ActionTypes.SET_DATA,
+            payload: { data: response.data },
+        });
     } catch (err) {
-    //   dispatch(setHasError(true));
-    console.log(err)
+          dispatch(setHasError(true));
     } finally {
-    //   dispatch(setIsLoading(false));
+          dispatch(setIsLoading(false));
     }
-  };
+};
+
+
+export const setIsLoading = (isLoading: boolean) => {
+    return {
+        type: ActionTypes.SET_LOADING,
+        payload: { isLoading }
+    }
+}
+
+export const setHasError = (hasError: boolean) => {
+    return {
+        type: ActionTypes.SET_HAS_ERROR,
+        payload: { hasError }
+    }
+}
